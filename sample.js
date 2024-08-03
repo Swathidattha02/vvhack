af2ca2e03ceaa14772ed9621f247ef961edcfb66
jayavarapulikhitha <[email>
2024-08-03 16:49:47 +0530

    ok

---
+++ sample.js
@@ -0,0 +1,22 @@
+// document.getElementById("dattha").innerHTML="<h1>lakshmi</h1>";
+// document.getElementById("dattha").textContent=="<h1>lakshmi</h1>";
+
+// document.getElementsByClassName("swathi")[1].textContent="<h1>swathidattha</h1>"
+// // document.getElementsByTagName
+//  Document.queryselector("#dattha")
+//  document.querySelectorAll(".swathi")
+
+//  const sravya = document.createElement('qwerty'); // Pass 'div' as a string
+// sravya.innerHTML = "srkrclg"; // Set the inner HTML
+// document.body.appendChild(sravya); // Append the div to the body
+ let a=10;
+console.log(a)
+let arr=[30,{key:"value"},"swathi",2123]
+console.log(arr)
+console.log(typeof(arr))
+
+// let object={NAME:"swathi",sec:"b",branch:"it"}
+// console.log(object.NAME&&"no data");
+let obj={name:"swathi",sec:"b",branch:"it"}
+console.log(object.name&&"no data");
+let string={one:console.log("swathi"),two:console.log("dattha")}
